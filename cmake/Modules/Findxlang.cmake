9fbf9e4eb7ffc9f50cb984457e4cefb6c588a22a
xlangdev <xlang@xlangfoundation.org>
2025-05-02 13:38:16 -0700

    add xlang project

---
+++ cmake/Modules/Findxlang.cmake
@@ -0,0 +1,13 @@
+
+FIND_PATH(XLANG_INCLUDE_DIR NAMES "xlang.h"
+                           PATHS ../xlang/Api ../../XF/xlang/Api
+                           ENV INCLUDE DOC "Directory containing xlang.h include file")
+
+
+IF (XLANG_INCLUDE_DIR)
+   SET(XLANG_FOUND TRUE)
+   get_filename_component(XLANG_DIR ${XLANG_INCLUDE_DIR} DIRECTORY)
+    message(STATUS "XLANG_INCLUDE_DIR=${XLANG_INCLUDE_DIR},and XLANG=${XLANG_DIR}")
+ELSE()
+    message(STATUS "xlang: not found")
+ENDIF (XLANG_INCLUDE_DIR)
